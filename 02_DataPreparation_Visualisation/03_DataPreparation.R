2494790d327f9f21532a7bae2304085e5f404110
ERICA RAVARELLI <ravarellierica@gmail.com>
2022-03-06 22:15:50 +0100

    tibbles for data viasualisation

--- 02_DataPreparation_Visualisation/03_DataPreparation.R
+++ 02_DataPreparation_Visualisation/03_DataPreparation.R
@@ -1,10 +1,33 @@
-table(geip17$genere, geip17$gruppo)
+##geip17----------
+
+prop.table(table(geip17$genere, geip17$gruppo), margin=1)
 
 barplot <- tibble(
   genere=geip17$genere,
-  grparl= geip17$gruppo
+  gruppo= geip17$gruppo
 )
 
-count <- barplot %>%
-  count(genere, grparl)
+count17 <- barplot %>%
+  count(genere, gruppo)
 count
+
+##tot2015----------
+
+prop.table(table(tot2015$genere, tot2015$donne), margin=1)
+
+boxplot <- tibble(
+  genere=tot2015$genere,
+  gruppo=tot2015$gruppo,
+  donne=tot2015$donne,
+  num.gender=tot2015$num.gender
+)
+
+count2015 <- boxplot %>%
+  count(genere, gruppo, donne, num.gender) 
+count2015
+
+count2015f <- filter(
+  count2015,
+  donne %in% "1"
+)
+

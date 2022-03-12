7ddd2fb8869eb07894eec7b58f83507124d1ed0a
ERICA RAVARELLI <ravarellierica@gmail.com>
2022-03-12 16:08:21 +0100

    report: remove obs data cleaning: change order

--- 01_DataImport_Cleaning/02_DataCleaning.R
+++ 01_DataImport_Cleaning/02_DataCleaning.R
@@ -1,8 +1,6 @@
 ##geip17 (gender equality italian parliament XVII legislature)---------------
 library(here)
 library(vroom)
-library(stringr)
-library(tidyverse)
 library(lubridate)
 library(tidytext)
 library(tidyr)
@@ -17,30 +15,84 @@ geip17$data_intervento <- ymd(geip17$data_intervento)
 geip17$inizioIncarico <- ymd(geip17$inizioIncarico)
 geip17$fineIncarico <- ymd(geip17$fineIncarico)
 
+##recode 'gruppo parlamentare'
+
+table(geip17$gruppo_parlamentare)
+
+##5stelle
+m5s <- str_detect(geip17$gruppo_parlamentare, "STELLE")
+m5s
+
+##misto
+mis <- str_detect(geip17$gruppo_parlamentare, "MISTO")
+mis
+
+##sinistra: articolo 1-movimento democratico e progressista, partito democratico, sinistra ecologia libertà, sinistra italiana
+sx <- str_detect(geip17$gruppo_parlamentare, "SINISTRA|DEMOCRATICO")
+sx
+
+##centro: alternativa popolare-centristi per l'europa, per l'Italia
+cen <- str_detect(geip17$gruppo_parlamentare, "CIVIC|POPOLARE|PER")
+cen
+
+##destra: forza italia-il popolo della libertà-Berlusconi presidente, fratelli d'Italia-alleanza nazionale, il popolo della libertà-Berlusconi presidente, lega nord e autonomie, nuovo centrodestra
+
+dx <- str_detect(geip17$gruppo_parlamentare, "POPOLO|FRATELLI|LEGA|DESTRA")
+dx
+
+
+##generate the variable gruppo_parl with the parliamentary groups
+
+gruppo_parl <- ifelse(m5s == TRUE,"M5S",
+                      ifelse(mis == TRUE, "Misto",
+                             ifelse(sx == TRUE, "Sinistra",
+                                    ifelse(cen==TRUE, "Centro",
+                                           ifelse(dx==TRUE, "Destra",
+                                                  "altro")))))
+
+
+##chek if I miss some categories 
+str_which(gruppo_parl, "altro")
+
+##add 'gruppo_parl' to the data frame geip17
+geip17$gruppo <- gruppo_parl
+with(geip17, table(gruppo_parlamentare, gruppo_parl))
+
 ##check if I selected speeches in which 'donne/a' was mentioned, as I wanted
 
 check_strings <- str_detect(geip17$argomento, "donn(e|a)")
 check_strings
 
-#I have some falses, so I need to check why and how to fix the problem
+#I have some falses, so I need to remove observations in which deputies don't talk about women
+
+to_remove <- str_match_all(geip17$argomento, "Renato Di Donna|Salvatore Caradonna|Madonna")
+to_remove
+
+to_remove <- tibble(
+  x1=1:366,
+  x2=to_remove
+)
+View(to_remove)
+
+geip17 <- filter(
+  geip17,
+  to_remove$x2 %in% "character(0)"
+)
+
 
 tm::stopwords("italian")
 stop_words
 stopwords
 
-##list other words I don't want to include in the analysis
+##list other words I don't want to include in the analysis and remove numbers from the variable 'argomento'
 
-manual_swords <-  c("d", "n", "3", "2", "02428", "00538", 
-                    "00544", "165", "7", "8", "00185", "90", "29", "00083", "1", 
-                    "00065", "00042", "00043", "00041", "00040", "00036", "00039",
-                    "90", "00110", "5", "00269", "01324", "04892", "00185", "00988",
-                    "1556", "s", "1278", "3354", "3359", "0139", "10456", "9", "08834", "09428",
-                    "281", "23", "00338", "00083", "01324", "00395", "10986",
-                    "caradonna", "madonna", "Renato Di", "nonch")
+geip17$argomento <- removeNumbers(geip17$argomento)
+
+manual_swords <-  c("d", "n", "s", "caradonna", "madonna", "Renato Di", "nonch")
 comb_mswords <- c(manual_swords, stopwords(kind="it"))
 comb_mswords
 
-##create a data frame with all the stop words I don't want to include
+##create a data frame with all the words I don't want to include
 
 manualswords <- tibble(
   x1=comb_mswords
@@ -84,64 +136,6 @@ bigrams_count <- bigramsfilter %>%
   count(noswords)
 bigrams_count
 
-##remove observations in which deputies don't talk about women
-
-to_remove <- str_match_all(geip17$argomento, "Renato Di Donna|Salvatore Caradonna|Madonna")
-to_remove
-
-toremove <- tibble(
-  x1=1:366,
-  x2=to_remove
-)
-View(toremove)
-
-geip17 <- filter(
-  geip17,
-  to_remove %in% "character(0)"
-)
-                 
-##recode 'gruppo parlamentare'
-
-table(geip17$gruppo_parlamentare)
-
-##5stelle
-m5s <- str_detect(geip17$gruppo_parlamentare, "STELLE")
-m5s
-
-##misto
-mis <- str_detect(geip17$gruppo_parlamentare, "MISTO")
-mis
-
-##sinistra: articolo 1-movimento democratico e progressista, partito democratico, sinistra ecologia libertà, sinistra italiana
-sx <- str_detect(geip17$gruppo_parlamentare, "SINISTRA|DEMOCRATICO")
-sx
-
-##centro: alternativa popolare-centristi per l'europa, per l'Italia
-cen <- str_detect(geip17$gruppo_parlamentare, "CIVIC|POPOLARE|PER")
-cen
-
-##destra: forza italia-il popolo della libertà-Berlusconi presidente, fratelli d'Italia-alleanza nazionale, il popolo della libertà-Berlusconi presidente, lega nord e autonomie, nuovo centrodestra
-
-dx <- str_detect(geip17$gruppo_parlamentare, "POPOLO|FRATELLI|LEGA|DESTRA")
-dx
-
-
-##generate the variable gruppo_parl with the parliamentary groups
-
-gruppo_parl <- ifelse(m5s == TRUE,"M5S",
-                             ifelse(mis == TRUE, "Misto",
-                                    ifelse(sx == TRUE, "Sinistra",
-                                           ifelse(cen==TRUE, "Centro",
-                                                 ifelse(dx==TRUE, "Destra",
-                                                        "altro")))))
-
-
-##chek if I miss some categories 
-str_which(gruppo_parl, "altro")
-
-##add 'gruppo_parl' to the data frame geip17
-geip17$gruppo <- gruppo_parl
-with(geip17, table(gruppo_parlamentare, gruppo_parl))
 
 
 ##tot2015---------------------------------
